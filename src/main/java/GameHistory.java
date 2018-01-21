ac052b941b9b7035f3006554cad5548dc6f2d992
ikeblaster <ike@thez.info>
2018-01-21 02:35:52 +0100

    Generalize Map

--- src/main/java/GameHistory.java
+++ src/main/java/GameHistory.java
@@ -1,10 +1,7 @@
 import ethnicity.EthnicGroup;
 import ethnicity.Ethnicity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -27,7 +24,7 @@ public class GameHistory {
         /**
          * Trust levels towards other ethnic groups.
          */
-        HashMap<Ethnicity,Double> trustLevels;
+        Map<Ethnicity,Double> trustLevels;
     }
 
     /**
